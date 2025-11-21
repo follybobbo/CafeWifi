2603797f1c9b452f761aa4352cace2ab05f4896f
follybobbo <follybobbo@icloud.com>
2025-11-21 17:24:49 +0000

    create review database table.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, Boolean, Float
+from sqlalchemy import Integer, String, Boolean, Float, ForeignKey
 from forms import SearchVenue
 from forms import VenueInfo
 import secrets
@@ -51,7 +51,8 @@ class Cafe(db.Model):
     latitude: Mapped[float] = mapped_column(Float, nullable=False)
     longitude: Mapped[float] = mapped_column(Float, nullable=False)
     country: Mapped[str] = mapped_column(String(250), nullable=False)
-    user_review: Mapped[List["Review"]] = relationship(back_populates="cafe_details") #lets see
+    user_review: Mapped["Review"] = relationship(back_populates="input_restaurant")
+
     # has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
     # has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
     # has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
@@ -63,6 +64,38 @@ class Cafe(db.Model):
 
 class Review(db.Model):
     __tablename__ = "review"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    wifi: Mapped[str] = mapped_column(String(250), nullable=False)
+    power_sockets: Mapped[str] = mapped_column(String(250), nullable=False)
+    length_of_work: Mapped[str] = mapped_column(String(250), nullable=False)
+    tables_and_chairs: Mapped[str] = mapped_column(String(250), nullable=False)
+    is_it_quiet: Mapped[str] = mapped_column(String(250), nullable=False)
+    audio_and_video: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    other_people_working: Mapped[str] = mapped_column(String(250), nullable=False)
+    group_tables: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    coffee_available: Mapped[str] = mapped_column(String(250), nullable=False)
+    food_offered: Mapped[str] = mapped_column(String(250), nullable=False)
+    veggie_options: Mapped[str] = mapped_column(String(250), nullable=False)
+    alcohol_offered: Mapped[str] = mapped_column(String(250), nullable=False)
+    credit_cards: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    natural_light: Mapped[str] = mapped_column(String(250), nullable=False)
+    outdoor_area: Mapped[str] = mapped_column(String(250), nullable=False)
+    how_large: Mapped[str] = mapped_column(String(250), nullable=False)
+    restroom: Mapped[str] = mapped_column(String(250), nullable=False)
+    wheelchair_accessible: Mapped[str] = mapped_column(String(250), nullable=False)
+    air_conditioned: Mapped[str] = mapped_column(String(250), nullable=False)
+    smoke_free: Mapped[str] = mapped_column(String(250), nullable=False)
+    pet_friendly: Mapped[str] = mapped_column(String(250), nullable=False)
+    parking_space: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    summary: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+    restaurant_id_id: Mapped[int] = mapped_column(ForeignKey("cafe.id"))
+    input_restaurant: Mapped["Cafe"] = relationship(back_populates="user_review")
 
 
 
@@ -172,6 +205,8 @@ def review_venue_info():
         form_csrf = request.form.get("csrf_token")
         if session_csrf_token != form_csrf:
             return 'CSRF token is missing or invalid', 400
+
+
 
         # WRITE ALL NECCESSARY INFO TO DATA BASE
     location_name = session.get("location_name", "")
