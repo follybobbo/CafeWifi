0bfbb7b22294d6da324d77704ac15229316f179e
follybobbo <follybobbo@icloud.com>
2025-11-24 00:21:16 +0000

    Successfully configure one to one relationship between Cafe and Review Tables

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
@@ -7,12 +7,12 @@ from forms import SearchVenue
 from forms import VenueInfo
 import secrets
 import os
+import re
 from typing import List
 from reviewquestions import survey_data
 
 # from flask_bootstrap import Bootstrap5
 
-print(survey_data)
 
 
 
@@ -30,7 +30,7 @@ db = SQLAlchemy(model_class=Base)
 
 #CONFIGURE THE EXTENSION: Connect extension to flask app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafess.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
 
 db.init_app(app)
 
@@ -47,6 +47,7 @@ class Cafe(db.Model):
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     # map_url: Mapped[str] = mapped_column(String(500), nullable=False)
     img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    city: Mapped[str] = mapped_column(String(250), nullable=False)
     address: Mapped[str] = mapped_column(String(250), nullable=False)
     latitude: Mapped[float] = mapped_column(Float, nullable=False)
     longitude: Mapped[float] = mapped_column(Float, nullable=False)
@@ -94,7 +95,7 @@ class Review(db.Model):
     summary: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
-    restaurant_id_id: Mapped[int] = mapped_column(ForeignKey("cafe.id"))
+    restaurant_id: Mapped[int] = mapped_column(ForeignKey("cafe.id"))
     input_restaurant: Mapped["Cafe"] = relationship(back_populates="user_review")
 
 
@@ -109,6 +110,16 @@ GOOGLE_PLACES_API_KEY = os.environ.get("API")
 
 
 
+def slugify(text):
+    stringed_text = str(text)
+    text = stringed_text.replace(" ", "-")
+
+    return text
+
+def de_slugify(slugified_text):
+    normalised_text = str(slugified_text).replace("-", " ")
+
+    return normalised_text
 
 
 
@@ -125,10 +136,13 @@ GOOGLE_PLACES_API_KEY = os.environ.get("API")
 def home():
     #Reads DB and stores location in list, conditional statement ensures there is no repetition of location
     cafe = db.session.execute(db.select(Cafe).order_by(Cafe.id)).scalars()
+
     cafes = cafe.all()
+
     location_list = []
     for items in cafes:
-        location = items.location
+        location = items.city
+        #TO AVOID REPETITION OF LOCATION
         if location not in location_list:
             location_list.append(location)
 
@@ -151,8 +165,6 @@ def add_place():
     if request.method == "POST" and step == 1:
 
         #store details in sessions.
-
-        # form_venue_info.name.data = location_name
         session["location_name"] = form_search_venue.location.data
         session["picture_url"] = form_search_venue.photo.data
         session["country"] = form_search_venue.country.data
@@ -161,6 +173,8 @@ def add_place():
         session["longitude"] = form_search_venue.longitude.data
         session["latitude"] = form_search_venue.latitude.data
         session["step"] = 2
+
+        #CHECK FOR PRESENCE OF CAFE
 
 
 
@@ -171,34 +185,45 @@ def add_place():
         session["location_name"] = form_venue_info.name.data
         session["street_name"] = form_venue_info.street.data
 
-        # print(session.get("location_name"))
-        # print(session.get("picture_url"))
-        # print(session.get("street_name"))
-        # print(session.get("longitude"))
-        # print(session.get("latitude"))
-        # print(session.get("country"))
+
 
         # store details in sessions.
-        new_cafe = Cafe(
-            name= session.get("location_name"),
-            img_url= session.get("picture_url"),
-            address= session.get("street_name"),
-            latitude= session.get("longitude"),
-            longitude= session.get("latitude"),
-            country= session.get("country")
-        )
+        #HANDLE IF RESTAURANT ALREADY EXISTS
 
-        db.session.add(new_cafe)
-        db.session.commit()
+        cafe_instance = db.session.execute(db.select(Cafe).where(Cafe.name == session["location_name"])).scalar()
 
-        #then clear session
-        return redirect(url_for("review_venue_info"))
+        if cafe_instance is not None:
+            #MEANS CAFE ALREADY EXISTS.
+            #FLASH MESSAGE
+            print("dont store")
+        else:
+            #IF RESTAURANT DOESN'T EXIST SAFE NEW RESTAURANT TO DATA BASE
+
+            new_cafe = Cafe(
+                name= session.get("location_name"),
+                img_url= session.get("picture_url"),
+                city= session.get("city"),
+                address= session.get("street_name"),
+                latitude= session.get("latitude"),
+                longitude= session.get("longitude"),
+                country= session.get("country")
+            )
+
+            db.session.add(new_cafe)
+            db.session.commit()
+
+
+            str_city = slugify(session.get("city"))
+            str_location_name = slugify(session.get("location_name"))
+
+            #then clear session
+            return redirect(url_for("review_venue_info", city=str_city, cafe_name=str_location_name))
 
 
     if step == 1:
         return render_template("add.html", form=form_search_venue, api_key=GOOGLE_PLACES_API_KEY)
     elif session["step"] == 2:
-        print(step)
+        # print(step)
         location_name = session.get("location_name", "")
         street_name = session.get("street_name", "")
         return render_template("add_venue.html", form=form_venue_info, name_of_venue=location_name, street=street_name, step=step)
@@ -216,13 +241,24 @@ def add_place():
 #
 #     return render_template("show-venue.html", cafes_list=cafes_list, location=location)
 
-@app.route("/review", methods=["POST", "GET"])
-def review_venue_info():
+@app.route("/<path:city>/<path:cafe_name>/review", methods=["POST", "GET"])
+def review_venue_info(city, cafe_name):
+    normal_cafe_name = de_slugify(cafe_name)
+
+    cafe_instance = db.session.execute(db.select(Cafe).where(Cafe.name == normal_cafe_name)).scalar()
+    cafe_id = cafe_instance.id
+    # print(cafe_id)
+    #
+    #
+    # print(f"city {city}")
+    # print(f"cafe {cafe_name}")
+    #GET ID OF CAFE AND USE TO STORE IN DB
 
     if request.method == "POST":
         # Validates CSRF FORGERY
         session_csrf_token = session.get("csrf_token")
         form_csrf = request.form.get("csrf_token")
+
         if session_csrf_token != form_csrf:
             return 'CSRF token is missing or invalid', 400
 
@@ -254,6 +290,8 @@ def review_venue_info():
         parking_space = request.form.get("parking-space")
 
         summary = request.form.get("summary")
+        cafe_instance = db.session.execute(db.select(Cafe).where(Cafe.name == normal_cafe_name)).scalar()
+        cafe_id = cafe_instance.id
 
         # STORE VARAIBLES IN DATABASE INSTANCE
         new_review = Review(
@@ -279,22 +317,68 @@ def review_venue_info():
             smoke_free=smoke_free,
             pet_friendly=pet_friendly,
             parking_space=parking_space,
-            summary=summary
+            summary=summary,
+            restaurant_id=cafe_id
         )
 
         # ADDS TO DATABASE
         db.session.add(new_review)
         db.session.commit()
 
+        return redirect(url_for("home"))
+
+
         # WRITE ALL NECCESSARY INFO TO DATA BASE
     location_name = session.get("location_name", "")
     csrf_token = secrets.token_hex(16)
     session["csrf_token"] = csrf_token
 
-    return render_template("review.html", location=location_name, question_bank=survey_data, csrf_token=csrf_token)
+    return render_template("review.html", location=location_name, csrf_token=csrf_token, city=city, cafe=cafe_name)
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+"""SHOW VENUE"""
+"""WRITE LOGIC TO COVERT REVIEW INPUT TO CUSTOM STYLES.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
