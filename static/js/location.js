def9c65ffab9509f67898f78cd75b981667fecbf
follybobbo <follybobbo@icloud.com>
2026-01-11 23:09:11 +0000

    fix color chnage effect on hover and click event on review buttons

--- static/js/location.js
+++ static/js/location.js
@@ -10,13 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //  hover effect on reaction buttons
-
+  let clicked_list = []
   let reactionEmojiList = document.querySelectorAll(".unto i");
+     //sets state of buttons. ensures color does not change when user hovers or moves cursor over a clicked button
 
   reactionEmojiList.forEach(function (reactionEmoji, index) {
+       reactionEmoji.dataset.clicked = false;
+       let clicked = (reactionEmoji.dataset.clicked.toString() === "true");
+//       console.log(clicked + " outer check")
 
-       var clicked = false;
-       reactionEmoji.addEventListener("mouseenter", function () {
+       reactionEmoji.addEventListener("mouseover", function () {
+            clicked = (this.dataset.clicked.toString() === "true");
             if (!clicked) {
               let className = reactionEmoji.classList[1];
               let newName = fill(className);
@@ -26,7 +30,8 @@ document.addEventListener("DOMContentLoaded", function () {
             }
        });
 
-      reactionEmoji.addEventListener("mouseleave", function () {
+      reactionEmoji.addEventListener("mouseout", function () {
+            clicked = (this.dataset.clicked.toString() === "true");
             if (!clicked) {
               let className = reactionEmoji.classList[1];
               let newName = deFill(className);
@@ -37,23 +42,49 @@ document.addEventListener("DOMContentLoaded", function () {
       });
 
       reactionEmoji.addEventListener("click", function () {
-            clicked = !clicked; //when user clicks the opposite of the current value of click becomes its new value.
+//            clicked = !clicked; //when user clicks the opposite of the current value of click becomes its new value.
+            this.dataset.clicked = true;
+            clicked = (this.dataset.clicked.toString() === "true");
+            console.log(clicked)
 
             if (clicked) {
+
+              if (clicked_list.length == 1) {   //deals with instance where user is clicking another button when a button has already been clicked.
+                let alreadyClicked = clicked_list[0][0];
+                let indexAlreadyClicked = clicked_list[0][1];
+                clicked_list[0][0].dataset.clicked = false;
+                console.log(alreadyClicked + "already clicked")
+                let alreadyClickedClassName = alreadyClicked.classList[1];
+                let defillAlreadyClickedClassName = deFill(alreadyClickedClassName);
+                alreadyClicked.classList.replace(alreadyClickedClassName, defillAlreadyClickedClassName);
+                removeBackgroundColor(indexAlreadyClicked, alreadyClicked);
+
+                clicked_list.length = 0;
+
+              }
+
+              clicked_list.push([this, index]);
               let className = reactionEmoji.classList[1];
-//              let newName = fill(className);
-              this.classList.replace(className, className);
+              this.classList.replace(className, className); //dont need to fill or de-fill because on clicking user is already hovering hence fill class already applied.
               changeBackgroundColor(index, reactionEmoji);
-            } else {
+
+              //take that and put up there,
+              //store value of selection for calculation.
+
+            } else {   //if user double clicks to unselect button
+              console.log(clicked_list);
               let className = reactionEmoji.classList[1];
               let newName = deFill(className);
 
               this.classList.replace(className, newName);
               removeBackgroundColor(index, reactionEmoji);
+              clicked_list.length = 0;
             }
       });
   });
 });
+
+
 
 
 function applyChanges(classOf, borderToChang) {
