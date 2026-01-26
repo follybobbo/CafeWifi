289ff31c51e97d46b2c53cac6c2e8516d45433ce
follybobbo <follybobbo@icloud.com>
2026-01-26 09:21:08 +0000

    deactivate emoji after click, add ajax function to emoji click to update value of summary when user clicks

--- static/js/location.js
+++ static/js/location.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function () {
 
+  //LOAD PRESET VALUE OF SUMMARY
+
+
   let borderToChangeList = document.querySelectorAll(".tooltip-container a");
 
 
@@ -12,22 +15,27 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 //  hover effect on reaction buttons
   let clicked_list = []
+  let summaryValue = document.querySelector(".unto").classList[1];  //get value of summary
   let reactionEmojiList = document.querySelectorAll(".unto i");
      //sets state of buttons. ensures color does not change when user hovers or moves cursor over a clicked button
+  //THE EMOJI CLICKING LOGIC MECHANISM IS EXPLAINED BELOW:
+//  1. ADD CLICKED DATASET TO ALL reactionEmoji,
+
 
   reactionEmojiList.forEach(function (reactionEmoji, index) {
-       reactionEmoji.dataset.clicked = false;
-//       let clicked = (reactionEmoji.dataset.clicked.toString() === "true");
+//       reactionEmoji.classList.add("disabled-link")
+       reactionEmoji.dataset.clicked = false; //sets default value of data set on all reactionEmoji
+        //       let clicked = (reactionEmoji.dataset.clicked.toString() === "true");
 
 
        reactionEmoji.addEventListener("mouseover", function () {
             clicked = (this.dataset.clicked.toString() === "true");
             if (!clicked) {
               let className = reactionEmoji.classList[1];
-              let newName = fill(className);
+              let newName = fill(className);     //adds '-flll' to the existing class name if not filled e.g 'bi-heart' becomes 'bi-heart-fill'
 
-              this.classList.replace(className, newName);
-              changeBackgroundColor(index, reactionEmoji);
+              this.classList.replace(className, newName);   //replaces 'bi-heart' with 'bi-heart-fill' in classList: element.classList.replace(oldClass, newClass);
+              changeBackgroundColor(index, reactionEmoji);   //applies a color changing class depending on index.
             }
        });
 
@@ -35,59 +43,102 @@ document.addEventListener("DOMContentLoaded", async function () {
             clicked = (this.dataset.clicked.toString() === "true");
             if (!clicked) {
               let className = reactionEmoji.classList[1];
-              let newName = deFill(className);
+              let newName = deFill(className);   //removes '-flll' in the existing class name if filled e.g 'bi-heart-fill' becomes 'bi-heart'
 
-              this.classList.replace(className, newName);
-              removeBackgroundColor(index, reactionEmoji);
+              this.classList.replace(className, newName);  //replaces : element.classList.replace(oldClass, newClass);
+              removeBackgroundColor(index, reactionEmoji);  //removes background image from emoji
             }
       });
 
-      reactionEmoji.addEventListener("click", function () {
-//            clicked = !clicked; //when user clicks the opposite of the current value of click becomes its new value.
-            this.dataset.clicked = true;
-            clicked = (this.dataset.clicked.toString() === "true");
-            console.log(clicked)
+      reactionEmoji.addEventListener("click", async function () {
+
+            this.dataset.clicked = true;  //tracks if this was clicked or not.
+
+            clicked = (this.dataset.clicked.toString() === "true"); //converts to string then compare
+            let fillStatus = this.classList[3];
 
             if (clicked) {
 
-              if (clicked_list.length == 1) {   //deals with instance where user is clicking another button when a button has already been clicked.
-                let alreadyClicked = clicked_list[0][0];
-                let indexAlreadyClicked = clicked_list[0][1];
-                clicked_list[0][0].dataset.clicked = false;
-                console.log(alreadyClicked + "already clicked")
-                let alreadyClickedClassName = alreadyClicked.classList[1];
-                let defillAlreadyClickedClassName = deFill(alreadyClickedClassName);
-                alreadyClicked.classList.replace(alreadyClickedClassName, defillAlreadyClickedClassName);
-                removeBackgroundColor(indexAlreadyClicked, alreadyClicked);
+              //happens if user is clicking on an emoji that is not already clicked
+              if (clicked_list.length == 1) {
 
-                clicked_list.length = 0;
+                  //deals with instance where user is clicking another button when a button has already been clicked.
 
+                  let alreadyClicked = clicked_list[0][0];       //clickedList = [this -> <i class="bi bi-heart-fill rating-emoji med"></i>, index]
+                  let indexAlreadyClicked = clicked_list[0][1];  //index of previously clicked
+
+                  clicked_list[0][0].dataset.clicked = false;    //change clicked dataset value of previously clicked button
+
+
+                  //THIS SECTION OF CODE DEALS WITH DE-FILLING OR DE-COLOURING PREVIOUSLY CLICKED BUTTON
+                  let alreadyClickedClassName = alreadyClicked.classList[1];
+                  let defillAlreadyClickedClassName = deFill(alreadyClickedClassName);                       //returns de-filled background name
+                  alreadyClicked.classList.replace(alreadyClickedClassName, defillAlreadyClickedClassName);  //make
+                  removeBackgroundColor(indexAlreadyClicked, alreadyClicked);
+
+                  alreadyClicked.closest("span").classList.remove("disabled-link");                                         //once user clicks another button, remove disable class from previous button
+
+
+                  clicked_list.length = 0; //RESET CLICKED LIST
+                  console.log(`${clicked_list.length} clicked inside clicked_list length`)
               }
 
-              clicked_list.push([this, index]);
+
+              clicked_list.push([this, index]);              //add newly clicked button to clicked_list array
+
               let className = reactionEmoji.classList[1];
               this.classList.replace(className, className); //dont need to fill or de-fill because on clicking user is already hovering hence fill class already applied.
-              changeBackgroundColor(index, reactionEmoji);
+                                                            //i.e bi-heart-fill = bi-heart-fill {fill class is active as user is already hovering before clicked}
 
+              changeBackgroundColor(index, reactionEmoji);  //changes background color, naturally the fill class default background color is black
+              this.closest("span").classList.add("disabled-link")           //disable button once user clicks, so as to control user input
+
+              //MAKE AJAX REQUEST TO SERVER TO UPDATE VALUE OF SUMMARY
+
+
+              //adds emoji that has been clicked to display container in header
               let containerOfHeaderEmoji = document.querySelector("#emoji");
               let cloneOfEmoji = this.cloneNode(true);
-              cloneOfEmoji.style.fontSize = "22px";
+              cloneOfEmoji.style.fontSize = "16px";
               containerOfHeaderEmoji.replaceChildren();
               containerOfHeaderEmoji.appendChild(cloneOfEmoji);
 
-              //take that and put up there,
-              //store value of selection for calculation.
+              //AJAX FUNCTION TO UPDATE SUMMARY
+              let valueOfEmoji = reactionEmoji.id;  //ID STORES THE SUMMARY VALUE.
+              let nameOfRestaurant = document.querySelector(".location-name").innerText;
+              let response = updateSummary(valueOfEmoji, nameOfRestaurant);
+              console.log(response)
 
-            } else {   //if user double clicks to unselect button
-              console.log(clicked_list);
-              let className = reactionEmoji.classList[1];
-              let newName = deFill(className);
 
-              this.classList.replace(className, newName);
-              removeBackgroundColor(index, reactionEmoji);
-              clicked_list.length = 0;
+
+
             }
       });
+      //LOADS DEFAULT VALUE OF SUMMARY INTO THE EMOJI
+      let valueOfEmoji = reactionEmoji.id;
+
+      if (summaryValue == valueOfEmoji) {
+       //      let selectedEmoji = reactionEmoji;
+        clicked_list.push([reactionEmoji, index]);
+        console.log(clicked_list)
+        let className = reactionEmoji.classList[1];
+        let newName = fill(className);
+        reactionEmoji.classList.replace(className, newName);
+        changeBackgroundColor(index, reactionEmoji);
+        reactionEmoji.closest("span").classList.add("disabled-link");
+
+
+         //adds emoji that has been clicked to display container in header
+         let containerOfHeaderEmoji = document.querySelector("#emoji");
+         let cloneOfEmoji = reactionEmoji.cloneNode(true);
+         cloneOfEmoji.style.fontSize = "16px";
+         containerOfHeaderEmoji.replaceChildren();
+         containerOfHeaderEmoji.appendChild(cloneOfEmoji);
+
+
+
+      }
+
   });
 
 //  Report closed mechanism
@@ -100,12 +151,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 
 
-//  check for current value of button before assigning dataset
-//  if (valueOfClosedButton == "REPORT CLOSED") {
-//    closedButton.dataset.opened = true;
-//  } else {
-//    closedButton.dataset.opened = false;
-//  }
+
 //CHECK STATUS THEN DO EFFECT OR NOT
 
   let closedStatusData = await checkStatus(nameOfRestaurant)
@@ -121,37 +167,29 @@ document.addEventListener("DOMContentLoaded", async function () {
       addEffectOnRestaurantClose(closedButton, closedBanner, reactionEmojiList, borderToChangeList, updatePageButton);
   }
 
-//  IF USER CLICKS ON REPORT CLOSED BUTTON
+  //  IF USER CLICKS ON REPORT CLOSED BUTTON
   closedButton.addEventListener("click", async function(){
     let statusData = await checkStatus(nameOfRestaurant);
     let isOpened = statusData.status
 
 
-//    CHECK DATASET OF BUTTON
-//    isOpened = this.dataset.opened.toString() === "true"
+     //    CHECK DATASET OF BUTTON
     if (isOpened) {
-//      REPORT CLOSED
+     //      REPORT CLOSED
       let report_res = await reportClosedOrOpened(nameOfRestaurant);
       addEffectOnRestaurantClose(closedButton, closedBanner, reactionEmojiList, borderToChangeList, updatePageButton);
     } else {
       let report_res = await reportClosedOrOpened(nameOfRestaurant)
       removeEffectOnRestaurantOpen(closedButton, closedBanner, reactionEmojiList, borderToChangeList, updatePageButton);
-//      closedButton.textContent = "REPORT CLOSED";
-////      this.dataset.opened = true;
-////      REMOVE CLOSED BANNER
-//      closedBanner.classList.toggle("closed-display");
-//
-//      reactionEmojiList.forEach(function(obj, index){
-//        obj.classList.remove("disabled-link");
-//      });
-//      borderToChangeList.forEach(function(obj, ind) {
-//       obj.classList.remove("disabled-link")
-//      });
-//      updatePageButton.classList.remove("disabled-link");
 
     }
 
   });
+
+     //SELECTS PRE-SAVED VALUE ON LOAD.
+
+
+
 });
 
 
@@ -205,6 +243,8 @@ function removeBackgroundColor(index, Emoji) {
     Emoji.classList.remove("hig");
   }
 }
+
+
 
 async function checkStatus(nameOfRestaurant) {
   try{
@@ -279,5 +319,32 @@ function removeEffectOnRestaurantOpen(button, banner, emojiList, listOfBorderToC
     });
 
     updateButton.classList.remove("disabled-link");
+
+}
+
+
+
+
+async function updateSummary(summary, nameOfRestaurant) {
+  try {
+    response = await fetch(`/update/summary`, {
+    method: "PATCH",
+    headers: {
+         "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+    name: nameOfRestaurant,
+    summary_value: summary
+    })
+
+    });
+    if (!response.ok) {
+    throw new Error(`HTTP error ${response.status}`);
+    }
+    result = await response.json();
+    return result;
+  } catch(error) {
+    console.log(error);
+  }
 
 }
