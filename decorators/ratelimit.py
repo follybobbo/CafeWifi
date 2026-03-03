0565f0ebc79c1baca1da50ee3f95d9479c330466
follybobbo <follybobbo@icloud.com>
2026-03-03 03:29:22 +0000

    create logic for rate limiter

--- decorators/ratelimit.py
+++ decorators/ratelimit.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from flask import request, jsonify
 from services.redis_bucket import RedisBucket
 import redis
 
@@ -6,13 +7,28 @@ client = redis.Redis("localhost", 6379, 0, decode_responses=True)
 
 
 
-def login_rate_limiter(request):
+
+def login_rate_limiter(limit, rate):
     token_bucket = RedisBucket(client)
     def decorator(f):
         @wraps(f)
         def wrapper_function(*args, **kwargs):
+            user_ip = request.remote_addr
+            key = f"id: {user_ip}"
+            redis_response = token_bucket.run_redis_script(key=key, bucket_capacity=limit, rate_of_refill=rate, token_per_request=1)
+            token_left = redis_response[0]
+            request_allowed = redis_response[1]
 
-            return f(*args, **kwargs)
+
+            if not request_allowed:
+                response = jsonify({"error": "Too many request"})
+                response.status_code = 429
+
+
+            response = f(*args, **kwargs)
+            response.headers["X-RateLimit-Remaining"] = f"{int(request_allowed)}"
+            return response
+
         return wrapper_function
 
     return decorator
