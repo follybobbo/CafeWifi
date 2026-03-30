96e521b363bdbea2a9814677b6e47d3dfb64baf3
follybobbo <follybobbo@icloud.com>
2026-03-30 09:21:30 +0100

    update login logic

--- app/routes/unprotected_routes.py
+++ app/routes/unprotected_routes.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template, session, redirect, url_for, flash
 from app.services import (get_all_cafes_and_order_by_id, get_cafe_city_list, get_all_cafe_instance_by_location,
                           get_all_cafe_and_order_by_country, create_country_city_list_dictionary, get_user_by_email,
                           de_serializer, update_user_email_verification_status, create_new_user, make_token, send_mail,
-                          remove_certain_keys_session)
+                          remove_certain_keys_session, BackOff, redis_client, check_if_session_key_is_expired,
+                          remove_singular_key_from_session)
 from dotenv import load_dotenv
 import os
 from app import cache, limiter
@@ -10,6 +11,7 @@ from app.decorators import login_token_bucket_limiter
 from flask_login import login_user, logout_user
 from app.forms import LoginForm, RegisterForm
 import werkzeug.security
+import time
 
 
 load_dotenv()
@@ -18,6 +20,7 @@ load_dotenv()
 
 unprotected = Blueprint("unprotected", __name__)
 GOOGLE_PLACES_API_KEY = os.environ.get("API")
+back_off = BackOff(redis_client)
 
 
 
@@ -49,7 +52,7 @@ def home():
 
 
 
-@unprotected.route("/<location>")
+@unprotected.route("/city/<location>")
 @limiter.limit("10/minute")
 @limiter.limit("2/second")
 def show_venue(location):
@@ -78,33 +81,96 @@ def show_cities():
 @unprotected.route("/login", methods=["GET", "POST"])
 @login_token_bucket_limiter(limit=10, rate=5)
 def login():
+    #make sure cooldown only shows when user makes a get request
     login_form = LoginForm()
+    # failed_login = False
+    # print(session.get("expiry_time"), "expiry")
+    # print(time.time())
+
+    # 1 the cooldown key had already been set to expire after 3600 seconds in redis, the block of code below tracks that for the front end.
+    # 2 the expiry time has been set below at the moment the user makes the first failed attempt using (time of first
+    #   failure attempt + 3600) since the key expires 3600s after first creation
+    # 3 so on every get request to this route, the first thing that happens is to check if the key has expired
+    is_ttl_session_key_expired = check_if_session_key_is_expired("expiry_time")
+
+    #if key has expired in redis which is our main backend tracker/reinforcer, then the key is deleted from session
+    if is_ttl_session_key_expired:
+        remove_singular_key_from_session("expiry_time")
+
+    if session.get("redirect"):
+        session["redirect"] = False
+    else:
+        session["ttl"] = None
+
+
 
     if login_form.validate_on_submit():
+
         email = login_form.email.data
         password = login_form.password.data
-
         user = get_user_by_email(email)
 
-        # user = db.session.execute(db.select(User).where(User.email == email)).scalar_one_or_none()
+        user_backoff_key = f"auth:login:{user}"
+        user_cooldown_key = f"auth:login:{user}:cooldown"
+        #get if user is banned
 
+        #runs if user email supplied does not exist
         if not user:
-            flash("User Does not Exist", "error")
+            #back off here
+            flash("Invalid Credentials", "error")
             return  redirect(url_for("unprotected.register"))
+
+        #check if user entered has previously attempted a failed login and has be set to cooldown.
+        # this is where cooldown state is determined, not in redis
+        if redis_client.exists(user_cooldown_key):
+            time_remaining = redis_client.ttl(user_cooldown_key)
+             # x = redis_client.ttl(user_cooldown_key)
+            session["ttl"] = time_remaining
+            print(session.get("ttl"))
+
+            session["redirect"] = True
+            flash(f"you can't login now try again in {time_remaining}s", "error")
+            return redirect(url_for("unprotected.login"))
+
+
+
+        #check if blocked: send to account recovery/verification
+        #This will only run if user exist.
+        is_password = werkzeug.security.check_password_hash(user.password, password)
+
+        if not is_password:
+            #runs script to register failed login attempt and register cooldown
+            failed_attempt, delay = back_off.start_back_off(user_backoff_key)
+
+            if failed_attempt == 10:
+                pass
+                #ban user
+                #make write action to db.banned
+
+            # create id and count
+            #check subsequently, if user try 3 times give a cool off timer of 3 mins, 3 more times, 6 mins, then one more block off
+
+
+            flash(f"Invalid Details wait for {delay}s", "error")
+            #original delay, set here
+            session["ttl"] = delay
+            print(session.get("ttl"))
+            session["expiry_time"] = time.time() + 3600
+            session["redirect"] = True
+
+            return redirect(url_for("unprotected.login"))
         else:
-            is_password = werkzeug.security.check_password_hash(user.password, password)
+            flash("Login Success", "info")
+            login_user(user)
+            #delete user from black list and removes timer from session.
+            back_off.delete_black_list_record(user_backoff_key)
+            session.pop("ttl")
 
-            if not is_password:
-                flash("Invalid Details", "error")
-                return redirect(url_for("unprotected.login"))
-            else:
+            return redirect(url_for("protected.dashboard"))
 
-                flash("Login Success", "info")
-                login_user(user)
 
-                return redirect(url_for("protected.dashboard"))
 
-    return render_template("login.html", form=login_form)
+    return render_template("login.html", form=login_form, cooldown_time=session.get("ttl", None))
 
 
 @unprotected.route("/logout")
