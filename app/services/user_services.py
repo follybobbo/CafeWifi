bd4d50c6e5867678778d947ac2707fee21143c34
follybobbo <follybobbo@icloud.com>
2026-04-01 12:12:25 +0100

    add function to update user display picture url in user db

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -44,3 +44,12 @@ def create_user_data_for_dashboard(the_current_user):
 def update_user_email_verification_status(the_user, update_status: bool):
     the_user.verified = update_status
     db.session.commit()
+
+
+def update_user_display_picture(user_id: int, picture_path: str):
+    user = db.session.execute(db.select(User).where(User.id == user_id)).scalar_one_or_none()
+    if user:
+        user.profile_picture = picture_path
+        db.session.commit()
+
+
