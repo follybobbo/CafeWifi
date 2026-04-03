eec2fd83199428b715c2f6150cc51465076d77b1
follybobbo <follybobbo@icloud.com>
2026-04-03 12:27:14 +0100

    add new function to module, to remove singular key in session, used in login view function

--- app/services/session_clear_services.py
+++ app/services/session_clear_services.py
@@ -5,3 +5,8 @@ from flask import session
 def remove_certain_keys_session(session_key_list: list):
     for key in session_key_list:
         session.pop(key, None)
+
+
+def remove_singular_key_from_session(key: str):
+    session.pop(key, None)
+    #log it somewhere later
