283f145cdb1f1c4f1b54c0b08ba333e81f1657cf
follybobbo <follybobbo@icloud.com>
2026-04-07 09:47:33 +0100

    changes

--- app/routes/ajax_routes.py
+++ app/routes/ajax_routes.py
@@ -1,9 +1,10 @@
 from app.services import (check_if_user_can_resend_verification_email, redis_client, make_token, send_mail,
                           get_single_cafe_by_cafe_name, update_status_of_cafe_to_opened_closed, get_review_record_using_id,
-                          update_review_summary, get_all_cafes_and_order_by_id, get_all_cafe_instance_by_location, update_user_display_picture)
+                          update_review_summary, get_all_cafes_and_order_by_id, get_all_cafe_instance_by_location, update_user_display_picture,
+                          delete_existing_pictures_in_folder, save_display_picture)
 from app.Utils import is_photo_file_allowed
 from flask_login import current_user, login_required
-from flask import url_for, jsonify, Blueprint, request, session, flash, redirect, make_response
+from flask import url_for, jsonify, Blueprint, request, session, flash, redirect, make_response, current_app
 from werkzeug.utils import secure_filename
 import requests
 from dotenv import load_dotenv
@@ -151,7 +152,7 @@ def get_lat_and_long():
 
     return lat_long_list  #returns list of latitude and longitude of each location
 
-@ajax_rule.route("/change-display-picture", methods=["POST", "GET"])
+@ajax_rule.route("/change-display-picture", methods=["POST"])
 @login_required
 def change_display_picture():
     if request.method == "POST":
@@ -202,11 +203,7 @@ def change_display_picture():
                 # update_user_display_picture(current_user.id, path)
                 return redirect(url_for("protected.dashboard"))
 
-            response = make_response()
-            response.status_code = 200
-            response.json({"message": "picture updated successfully"})
 
-            return response
 
 
 
